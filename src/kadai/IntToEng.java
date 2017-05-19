8bb8196ad862b2e887e096e0c409dad0a72d7f88
g15920ka <g15920ka@s30401.tsuda.ac.jp>
2017-05-19 15:11:17 +0900

    0-1000まで表記にしました！

--- src/kadai/IntToEng.java
+++ src/kadai/IntToEng.java
@@ -7,9 +7,24 @@ public class IntToEng {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
-		System.out.println(translateEng(input));
+		if(input > 100){
+			System.out.println(translateEng1(input));
+		    }else {
+			System.out.println(translateEng(input));
+		    }
 	}
-	
+	static String translateEng1(int n){
+		String b = "";
+		//if(n >= 100){
+		    int x = n/100;
+		    int y = n - 100*x;
+		    b = translateEng(x) + " hundred " +  translateEng(y);
+		    //}
+		    if(n == 1000) {
+			b = "one thousand";
+		    }
+		return b;
+	    }
 	// 数値を英訳する変換するメソッド
 	static String translateEng(int n) {
 		String a = "";
@@ -18,12 +33,14 @@ public class IntToEng {
 				"eighteen", "nineteen"};
 		if(n < 20) {
 			a = suuji[n]; // 0から19まで
-		} else {
+		} else if(n < 100){
 			String[] suuji2 = {"twenty", "thirty", "forty", "fifty", "sixty","seventy", "eighty", "ninety"};
 			int b = n / 10; // 十の位	
 			int c = n - b * 10;
 			a = suuji2[b-2];
 			if (c != 0) a = a + " " + suuji[c];
+		} else {
+			a = "one hundred";
 		}
 		return a;
 	}
--- test/kadai/IntToEngTest.java
+++ test/kadai/IntToEngTest.java
@@ -1,5 +1,6 @@
 package kadai;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,11 +8,61 @@ import org.junit.Test;
 public class IntToEngTest {
 
 	@Test
-	public void test() {
+	public void test1() {
 		IntToEng ite = new IntToEng();
-		
+		String s = "nineteen";
+		String t = ite.translateEng(19);
+		assertThat(t,is(s));
 	}
-
+	@Test
+	public void test2(){
+		IntToEng ite = new IntToEng();
+		String s = "twenty";
+		String t = ite.translateEng(20);
+		assertThat(t,is(s));
+	}
+	@Test
+	public void test3(){
+		IntToEng ite = new IntToEng();
+		String s = "forty two";
+		String t = ite.translateEng(42);
+		assertThat(t,is(s));
+	}
+	@Test
+	public void test4(){
+		IntToEng ite = new IntToEng();
+		String s = "one hundred";
+		String t = ite.translateEng(100);
+		assertThat(t,is(s));
+	}
+	@Test
+	public void test5(){
+		IntToEng ite = new IntToEng();
+		String s = "one hundred one";
+		String t = ite.translateEng1(101);
+		assertThat(t,is(s));
+	}
+	@Test
+	public void test6(){
+		IntToEng ite = new IntToEng();
+		String s = "one hundred eleven";
+		String t = ite.translateEng1(111);
+		assertThat(t,is(s));
+	}
+	@Test
+	public void test7(){
+		IntToEng ite = new IntToEng();
+		String s = "one thousand";
+		String t = ite.translateEng1(1000);
+		assertThat(t,is(s));
+	}
+	@Test
+	public void test8(){
+		IntToEng ite = new IntToEng();
+		String s = "one";
+		String t = ite.translateEng(1);
+		assertThat(t,is(s));
+	}
 	/*
  	@Test
 	public void snakeToCamelcaseのテストその1() {
